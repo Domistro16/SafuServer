41d8bdd6ded48c5e3639e5030c9ec96c27a993a2
Desmond Egwurube <desmondesih@gmail.com>
2025-10-04 21:36:12 +0100

    Added new return values

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,12 @@ app.use(cors());
 
 app.use(express.json());
 const port = process.env.PORT || 800;
-
+console.log(
+  "routes type",
+  typeof routes,
+  Array.isArray(routes),
+  routes && routes.stack ? "is Router" : "not Router"
+);
 app.use("/api", routes);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
